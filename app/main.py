cd3ccbff38fa1c22c482bfd40a05efa35570db95
MeIsKhoa <lekhoa4856@gmail.com>
2025-05-08 13:47:43 +0700

    add Login api

--- app/main.py
+++ app/main.py
@@ -20,24 +20,6 @@ async def shutdown_event():
 app.include_router(auth_router)
 
 
-@app.on_event("startup")
-async def startup_event():
-    await init_db()
-    # Kiểm tra kết nối
-    try:
-        db = get_database()
-        await db.command("ping")
-        print("Kết nối tới MongoDB Atlas thành công!")
-    except Exception as e:
-        print(f"Lỗi kết nối tới MongoDB Atlas: {e}")
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    await close_db()
-    print("Đã đóng kết nối tới MongoDB Atlas.")
-
-
 @app.get("/")
 async def hello_world():
     return {"message": "Hello World"}
